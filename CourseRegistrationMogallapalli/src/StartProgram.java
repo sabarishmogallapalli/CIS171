74bc7ca3df9ba04c1ace1507cdedb6ccfe1af29d
sabarishmogallapalli <65688211+sabarishmogallapalli@users.noreply.github.com>
2022-09-03 18:14:53 -0500

    Add files via upload

---
+++ CourseRegistrationMogallapalli/src/StartProgram.java
@@ -0,0 +1,35 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import view.MainMenuJPanel;
+
+import javax.swing.JButton;
+import java.awt.GridLayout;
+
+/**  
+* Sabarish Mogallapalli - smogallapalli
+* CIS171 27114
+* Apr 26, 2022  
+*/
+public class StartProgram {
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		MainMenuJPanel panel = new MainMenuJPanel();
+		frame.add(panel);
+
+		frame.setSize(500, 320);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+}
